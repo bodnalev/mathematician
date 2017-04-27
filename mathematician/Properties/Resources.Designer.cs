10456c10e9292ad97106703e5fe58a3b3d954e4b
bodnalev <bodnalev@gmail.com>
2017-04-27 15:59:17 +0100

    Rename

--- mathematician/Properties/Resources.Designer.cs
+++ mathematician/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace mathematician.Properties {
+namespace Mathematician.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace mathematician.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("mathematician.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mathematician.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
